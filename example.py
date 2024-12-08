caf3f65e086596c703c1b3ef1726c8e842f7b9a9
Minsub Kim <kms300508@hanyang.ac.kr>
2024-12-08 20:03:32 +0900

    add sample code and sample images

---
+++ example.py
@@ -0,0 +1,30 @@
+from imgPreprocessor import *
+
+image_path = './example_img/4.jpg'
+
+#CustomBlurFilter
+filter = CustomBlurFilter(image_path)
+filter.load_image()
+filter.enhance_and_blur()
+filter.display_images()
+
+#ColorFilter
+filter = ColorFilter(image_path)
+user_color = (1, 255, 125)
+
+filter.custom_colorfilter(user_color)
+filter.show()
+
+filter.make_gray_image()
+filter.show()
+
+#color_extracter
+plot_palette((extract_color_palette(image_path, 3)))
+
+#CountFace
+face = CountFace()
+face.set_image_path("./example_img/person.jpg")
+face.count_faces()
+
+#modexif
+modexif("./example_img/test.jpg", "output_path.jpg", "S24", "2023:12:05 12:57:54")
